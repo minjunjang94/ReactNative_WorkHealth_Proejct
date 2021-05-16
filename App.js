afd0cdf6440033547f7f61906af3d0c3c8d0af6e
minjunjang94 <emr753@naver.com>
2021-05-16 12:14:59 +0900

    (init) create React_Native
    (init) create React_Native

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+ } from 'react-native';
+ 
+ const App: () => Node = () => {
+   return (
+       <View style = {styles.container}>
+           <Text style = {styles.hello}>Hello World</Text>
+       </View>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+     container : {
+       flex : 1, //View의 전체 영역을 말한다.
+       justifyContent : 'center', // 가로 영역 센터
+       alignItems : 'center' //세로 영역 센터
+     },
+     hello : {
+       color : 'red'
+     }
+ });
+ 
+ export default App;
+ 
